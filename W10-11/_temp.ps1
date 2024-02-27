2040c77b2d3098671347b95df69a49e6e8e90e5b
Vincent <1580359+AdminVin@users.noreply.github.com>
2024-02-27 12:30:56 -0500

    Update 2024-02-27

--- W10-11/_temp.ps1
+++ W10-11/_temp.ps1
@@ -1,68 +1,22 @@
-# Define the path for the .reg file
-$regFilePath = "C:\Temp\TakeOwnership.reg"
+# Define the URL of the .reg file
+$url = "https://github.com/AdminVin/W1X-Debloat/raw/main/W10-11/RegistryKeys/TakeOwnership_Pause.reg"
 
-# Check if C:\Temp exists, if not, create it
-if (-not (Test-Path "C:\Temp")) {
+# Define the download path
+$downloadPath = "C:\Temp\TakeOwnership_Pause.reg"
+
+# Ensure the Temp directory exists
+If (-not (Test-Path "C:\Temp")) {
     New-Item -ItemType Directory -Path "C:\Temp"
 }
 
-# Define the content of the .reg file
-$regFileContent = @"
-Windows Registry Editor Version 5.00
+# Use WebClient to download the .reg file
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($url, $downloadPath)
 
-; Created by: Shawn Brink
-; Created on: September 6, 2021
-; Updated on: January 7, 2024
-; Tutorial: https://www.elevenforum.com/t/add-take-ownership-to-context-menu-in-windows-11.1230/
-
-
-[-HKEY_CLASSES_ROOT\*\shell\TakeOwnership]
-[-HKEY_CLASSES_ROOT\*\shell\runas]
-
-[HKEY_CLASSES_ROOT\*\shell\TakeOwnership]
-@="Take Ownership"
-"Extended"=-
-"NoWorkingDirectory"=""
-"NeverDefault"=""
-"Icon"="imageres.dll,-5311"
-
-[HKEY_CLASSES_ROOT\*\shell\TakeOwnership\command]
-@="powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/c takeown /f \\\"%1\\\" && icacls \\\"%1\\\" /grant *S-1-3-4:F /t /c /l & pause' -Verb runAs\""
-"IsolatedCommand"= "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/c takeown /f \\\"%1\\\" && icacls \\\"%1\\\" /grant *S-1-3-4:F /t /c /l & pause' -Verb runAs\""
-
-
-
-[HKEY_CLASSES_ROOT\Directory\shell\TakeOwnership]
-@="Take Ownership"
-"AppliesTo"="NOT (System.ItemPathDisplay:=\"C:\\Users\" OR System.ItemPathDisplay:=\"C:\\ProgramData\" OR System.ItemPathDisplay:=\"C:\\Windows\" OR System.ItemPathDisplay:=\"C:\\Windows\\System32\" OR System.ItemPathDisplay:=\"C:\\Program Files\" OR System.ItemPathDisplay:=\"C:\\Program Files (x86)\")"
-"Extended"=-
-"NoWorkingDirectory"=""
-"Position"="middle"
-"Icon"="imageres.dll,-5311"
-
-[HKEY_CLASSES_ROOT\Directory\shell\TakeOwnership\command]
-@="powershell -windowstyle hidden -command \"$Y = ($null | choice).Substring(1,1); Start-Process cmd -ArgumentList ('/c takeown /f \\\"%1\\\" /r /d ' + $Y + ' && icacls \\\"%1\\\" /grant *S-1-3-4:F /t /c /l /q & pause') -Verb runAs\""
-"IsolatedCommand"="powershell -windowstyle hidden -command \"$Y = ($null | choice).Substring(1,1); Start-Process cmd -ArgumentList ('/c takeown /f \\\"%1\\\" /r /d ' + $Y + ' && icacls \\\"%1\\\" /grant *S-1-3-4:F /t /c /l /q & pause') -Verb runAs\""
-
-
-
-[HKEY_CLASSES_ROOT\Drive\shell\runas]
-@="Take Ownership"
-"Extended"=-
-"NoWorkingDirectory"=""
-"Position"="middle"
-"AppliesTo"="NOT (System.ItemPathDisplay:=\"C:\\\")"
-"Icon"="imageres.dll,-5311"
-
-[HKEY_CLASSES_ROOT\Drive\shell\runas\command]
-@="cmd.exe /c takeown /f \"%1\\\" /r /d y && icacls \"%1\\\" /grant *S-1-3-4:F /t /c & Pause"
-"IsolatedCommand"="cmd.exe /c takeown /f \"%1\\\" /r /d y && icacls \"%1\\\" /grant *S-1-3-4:F /t /c & Pause"
-"@
-
-# Write the content to the .reg file
-$regFileContent | Out-File -FilePath $regFilePath -Encoding ASCII
-
-
-#Start-Process "regedit.exe" -ArgumentList "/s `"$regFilePath`"" -Verb RunAs
-$command = "regedit /s `"$regFilePath`""
+# Execute the downloaded .reg file
+$command = "regedit /s `"$downloadPath`""
 Invoke-Expression $command
+
+
+# Clean up: Remove the .reg file after execution
+Remove-Item -Path $downloadPath -Force
