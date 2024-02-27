e95ec4172549dc800901ab8ce5b87363656b15e8
Vincent <1580359+AdminVin@users.noreply.github.com>
2024-02-27 12:39:29 -0500

    Update 2024-02-27

--- W10-11/_temp.ps1
+++ W10-11/_temp.ps1
@@ -2,11 +2,11 @@
 $url = "https://github.com/AdminVin/W1X-Debloat/raw/main/W10-11/RegistryKeys/TakeOwnership_Pause.reg"
 
 # Define the download path
-$downloadPath = "C:\Temp\TakeOwnership_Pause.reg"
+$downloadPath = "C:\ProgramData\AV\Temp\TakeOwnership_Pause.reg"
 
 # Ensure the Temp directory exists
-If (-not (Test-Path "C:\Temp")) {
-    New-Item -ItemType Directory -Path "C:\Temp"
+If (-not (Test-Path "C:\ProgramData\AV\Temp\")) {
+    New-Item -ItemType Directory -Path "C:\ProgramData\AV\Temp\"
 }
 
 # Use WebClient to download the .reg file
@@ -14,9 +14,8 @@ $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($url, $downloadPath)
 
 # Execute the downloaded .reg file
-$command = "regedit /s `"$downloadPath`""
-Invoke-Expression $command
+Start-Process "regedit.exe" -ArgumentList "/s", $downloadPath -Wait -NoNewWindow
 
 
 # Clean up: Remove the .reg file after execution
-Remove-Item -Path $downloadPath -Force
+Remove-Item -Path "C:\ProgramData\AV\Temp" -Recurse -Force
